4f5627259a8fa5a44d52bdf509e22d818ab2a2ab
Andrii Iatsyn <kornulios@Pachanga.local>
2018-01-22 16:47:25 +0100

    Added gitignore

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log('up and running');
+
+function player() {
+  this.speed = 10,
+  this.x = 0,
+  this.y = 0;
+}
+
+player.prototype.run = function() {
+  console.log('Im running with speed ' + this.speed);
+}
+
+player.prototype.setSpeed = function(speed) {
+  return this.speed = speed;
+}
+
+var p1 = new player();
+p1.setSpeed(20);
+
+p1.run();
