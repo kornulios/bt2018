22195224255b251b76b5f27c1d8812d190e52187
Andrii Iatsyn <kornulios@Pachanga.local>
2018-01-22 17:58:14 +0100

    added axios module

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 console.log('up and running');
 
+// var axios = require('axios');
+
+axios.get('http://localhost:3000/data').then(
+  function(res) {
+    console.log(res);
+    var p2 = new player();
+    p2.setSpeed(50);
+    p2.run();
+  }
+);
+
 function player() {
   this.speed = 10,
   this.x = 0,
