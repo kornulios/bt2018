8359f543a25756971acefdacd7b494df56b28cdc
Andrii Iatsyn <andriy.iatsyn@gmail.com>
2018-01-23 17:11:04 +0100

    Added timer

--- js/game.js
+++ js/game.js
@@ -3,16 +3,23 @@
 function Game() {
   var players = [];
 
-  this.initGame = function(numPlayers) {
+  this.initGame = function (numPlayers) {
     var newPlayers = numPlayers || 1;
-    for(var i=0; i<newPlayers; i++){
-      players.push(new Player({speed: Math.floor(Math.random() * 40 + 20)}));
+    for (var i = 0; i < newPlayers; i++) {
+      players.push(new Player({ speed: Math.floor(Math.random() * 40 + 20) }));
     }
   }
 
-  this.playGame = function() {
-    for (var i=0; i<players.length; i++) {
-      players[i].run();
-    }
+  this.playGame = function () {
+    var ticker;
+    var tickNum = 0, maxTicks = 5;
+    ticker = setInterval(function () {
+      console.log("Turn ", tickNum);
+      for (var i = 0; i < players.length; i++) {
+        players[i].run();
+      }
+      tickNum++;
+      if (tickNum > maxTicks) clearInterval(ticker);
+    }, 1000);
   }
 }
