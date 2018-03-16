4b524878c6917009fa993b81db60a489891dfe71
Andrii Iatsyn <andriy.iatsyn@gmail.com>
2018-03-16 16:22:06 +0100

    Cosmetics and bugfixes

--- js/race.js
+++ js/race.js
@@ -24,7 +24,7 @@ class Race {
     }
     //check race end
     for (let p of me.players) {
-      if (p.running) return true;
+      if (p.running || p.shooting) return true;
     }
     return false;
   }
@@ -48,77 +48,5 @@ class Race {
         p.addPenalty(track.penaltyLength);
       }
     }
-  }
-}
-
-
-function oldGame() {
-  var players = [],
-    track,
-    gameTimer = 0,
-    gameRuns = false,
-    gameSpeed = 100,
-    gameResults = {};
-
-  var gm = this;
-
-  this.playGame = function () {
-    var ticker;
-    var me = this;
-    var tickNum = 0, maxTicks = 5;
-    gameRuns = true;
-
-    //main game loop
-    ticker = setInterval(function () {
-
-      gameTimer += 0.1;
-
-      for (var i = 0; i < players.length; i++) {
-        var player = players[i];
-        var runStatus;
-        if (player.running) {
-          player.run();
-          runStatus = track.isWaypointPassed(player.getPlayer());
-          if (runStatus !== -1) {
-            results.pushResult(player.name, runStatus, gameTimer.toFixed(1));
-            if (runStatus == track.getWaypointsNum() - 1) player.stop();
-          }
-          if (track.passShootingRange(player.getPlayer())) {
-            player.shoot();
-          }
-        } else if (player.shooting) {
-          player.shoot();
-        }
-      }
-
-      //render graphics
-      me.renderPlayers();
-      renderResults(results);
-      renderGameTurn(tickNum);
-
-      gameRuns = me.checkGameEnd();
-
-      tickNum++;
-      if (!gameRuns) {
-        clearInterval(ticker);
-        setGameStatus('Game ended');
-        
-      }
-    }, gameSpeed);
-
-  }
-
-  this.checkGameEnd = function () {
-    for (i = 0; i < players.length; i++) {
-      if (players[i].getDistance() < track.getTrackLength()) return true;
-    }
-    return false;
-  }
-
-  this.renderPlayers = function () {
-    clearMainView();
-    players.forEach(function (player, i) {
-      renderPlayer(player);
-    });
   }
 }
