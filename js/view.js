a259a0eeea0b5c00bb36f8753049b809add51bc8
Andrii Iatsyn <andriy.iatsyn@gmail.com>
2018-03-23 16:55:12 +0100

    Housekeeping

--- js/view.js
+++ js/view.js
@@ -17,13 +17,12 @@ class View {
   }
 
   renderResults(results) { //should render sorted results per waypoint
-    // console.log(results);
     let me = this;
     let tpl = "";
     let place = 1;
     for (let r of results) {
       tpl += '<div class="row">';
-      tpl += `<div style="width:20px;">${place}</div>` + me.drawCell(r.playerName) + me.drawCell(r.time);
+      tpl += `<div style="width:20px;">${place}</div>` + me.drawCell(r.playerName) + me.drawCell((r.rTime));
       tpl += '</div>';
       place++;
     }
@@ -38,41 +37,4 @@ class View {
     return `<div>${text}</div>`;
   }
 
-}
-
-function renderResultsOld(results) {
-  var mainView = document.querySelector("#results-view");;
-  var players = results.playerNames();
-  var mainTpl = "";
-  var headerTpl = "<div class='head-row'>Name</div>";
-  for (var i = 0; i < results.maxWaypointNum(); i++) {
-    headerTpl += `<div>WP_${i}</div>`;
-  }
-
-  headerTpl = "<div class='row'>" + headerTpl + "</div>";
-
-  players.forEach(function (name) {
-    var rslt = results.getPlayerResults(name);
-    var resTpl = "";
-
-    rslt.forEach(function (r) {
-      resTpl += "<div>" + r.time + "</div>";
-    });
-    mainTpl += `<div class='row'><div>${name}</div>${resTpl}</div>`;
-  });
-  mainView.innerHTML = headerTpl + mainTpl;
-}
-
-function getMainView() {
-  return document.querySelector("#main-view");
-}
-
-function clearMainView() {
-  var mainView = getMainView();
-  mainView.innerHTML = "";
-}
-
-function setGameStatus(message) {
-  var view = document.querySelector("#turn-box");
-  view.innerHTML = message;
 }
