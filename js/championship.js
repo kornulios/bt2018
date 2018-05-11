3b57daec01d16b805a3c4cde54c689b0a6322906
Andrii Iatsyn <andriy.iatsyn@gmail.com>
2018-05-11 13:50:32 +0200

    Player list refactor; points remap

--- js/championship.js
+++ js/championship.js
@@ -6,7 +6,10 @@ class Championship {
     this.players = [];
     this.roster = [];   //TBI
     this.nextRace = 0;
-    this.pointsMap = [10, 6, 4, 3, 2, 1];
+    this.pointsMap = [60, 54, 48, 43, 40, 38, 36, 34, 32, 31,
+                    30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 
+                    20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 
+                    10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
     this.initPlayers(newPlayers);
     this.initRaces(raceConfigs);
