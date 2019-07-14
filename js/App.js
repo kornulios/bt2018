9a0945184e4e77fc2ed7281cc322c9a5aa20ce1a
Andrii Iatsyn <andriy.iatsyn@gmail.com>
2019-07-14 22:33:02 +0200

    Fixes

---
+++ js/App.js
@@ -0,0 +1,11 @@
+class App {
+    constructor() {
+        this.tabs = [];
+        this.name = 'Application'
+        this.activeTab = '';
+    };
+
+    static render(domNode) {
+        // console.log(domNode.innerHTML);
+    }
+}
