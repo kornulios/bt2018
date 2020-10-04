28c8dbf52f6538436d1b7965c1fd9b55c491d6a8
unknown <andriy.iatsyn@gmail.com>
2020-10-04 10:51:49 +0200

    Player Bub component

--- js/view/PlayerControls/PlayerControls.js
+++ js/view/PlayerControls/PlayerControls.js
@@ -1,9 +1,12 @@
+import { PlayerBub } from "../PlayerBub/PlayerBub.js";
+
 export const PlayerControls = (player) => {
   const playerHtml = `<div class="player-control-item">
     <div class="player-control-portrait">
-      <div>${player.number}</div>
+      IMG
     </div>
     <div class="player-control-section">
+      <div>${PlayerBub(player)}</div>
       <div>${player.name}</div>
     </div>
     <div class="player-control-section">
