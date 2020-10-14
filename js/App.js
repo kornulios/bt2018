db0165f440f880a4bc010958dc06159c7ccbece8
kornulios <andriy.iatsyn@gmail.com>
2020-10-14 10:51:41 +0200

    Game init refactor

--- js/App.js
+++ js/App.js
@@ -1,31 +1,29 @@
-import { Game } from "./game.js";
+import { Game } from "./game";
 
-const game = new Game();
-
-const initEvents = () => {
-  document.querySelector("#start-race").addEventListener("click", game.onStartRaceClick.bind(game));
-
-  // document
-  //   .querySelector("#race-indi")
-  //   .addEventListener("click", game.simulateSprint.bind(game));
-
-  // document
-  //   .querySelector("#race-relay")
-  //   .addEventListener("click", game.simulateRelay.bind(game));
-
-  // document
-  //   .querySelector("#race-mass")
-  //   .addEventListener("click", game.simulatePlayer.bind(game));
-
-  document.querySelector("#champ-races").addEventListener("click", game.showChampionshipStandings.bind(game));
-
-  document.querySelector("#pause").addEventListener("click", game.pauseGame.bind(game));
-
-  document.querySelector("#skip-race").addEventListener("click", game.onSimulateRaceClick.bind(game));
-
-  document.querySelector("#results-controls").addEventListener("click", game.onResultSelect.bind(game));
-  document.querySelector("#results-paging").addEventListener("click", game.onResultPageSelect.bind(game));
-};
-
-document.addEventListener("DOMContentLoaded", initEvents);
 //champ-standings
+
+export class App {
+  constructor() {
+    this.game = null;
+  }
+
+  initEvents = () => {
+    document.querySelector("#start-race").addEventListener("click", this.game.onStartRaceClick.bind(this.game));
+
+    document
+      .querySelector("#champ-races")
+      .addEventListener("click", this.game.showChampionshipStandings.bind(this.game));
+
+    document.querySelector("#pause").addEventListener("click", this.game.pauseGame.bind(this.game));
+
+    document.querySelector("#skip-race").addEventListener("click", this.game.onSimulateRaceClick.bind(this.game));
+
+    document.querySelector("#results-controls").addEventListener("click", this.game.onResultSelect.bind(this.game));
+    document.querySelector("#results-paging").addEventListener("click", this.game.onResultPageSelect.bind(this.game));
+  };
+
+  start() {
+    this.game = new Game();
+    this.initEvents();
+  }
+}
